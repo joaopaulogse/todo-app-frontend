9e5596cd294e7f87157831dfb764b89379746cd9
Joao Paulo <joaopaulogse@gmail.com>
2017-09-24 16:55:56 -0300

    quase concluindo o module de react com reduDx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import {render} from 'react-dom'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
+
+import multi from 'redux-multi'
+import promise from 'redux-promise'
+import thunk from 'redux-thunk'
 
 import App from './main/app'
 import reducers from './main/reducers'
 
-const store = createStore(reducers)
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
 render(
     <Provider store={store}>
         <App/>
--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,22 +1,6 @@
 import { combineReducers } from 'redux'
-
+import todoReducer from '../todo/todoReducer'
 const rootReducers = combineReducers({
-    todo: ()=>({
-        description:'Ler um Livro',
-        list:[{
-            _id:1,
-            description:'Pagar fatura do cartão',
-            done:true
-        },{
-            _id:2,
-            description:'Reuniao com a equipe as 10:00',
-            done:false
-        },{
-            _id:3,
-            description:'Consulta medica na terça depois do almoço',
-            done:false
-        }]
-    })
+    todo: todoReducer
 })
-
 export default rootReducers
